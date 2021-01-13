6b04c81a2b30e2ce8e7784a41013a533f8a74242
Duncan <duncanmarten@yahoo.com>
2021-01-13 10:09:41 -0600

    added models and controllers for user and thought

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(v);
+                },
+                message: given => `${given.value} is not a valid email address!`
+            }
+        },
+        thoughts: [
+            {
+                ref: 'Thought'
+            }
+        ],
+        friends: [UserSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
